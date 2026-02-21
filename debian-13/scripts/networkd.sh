ac99af85179029f249765e176a1a77f59a1e3182
Kris Lamoureux <kris@lamoureux.io>
2026-02-21 09:02:16 -0500

    Delegate DNS to the host via the gateway IP

--- debian-13/scripts/networkd.sh
+++ debian-13/scripts/networkd.sh
@@ -19,5 +19,4 @@ mv /etc/network/interfaces.d /etc/network/interfaces.d.save
 systemctl enable systemd-networkd
 systemctl disable networking
 
-printf "nameserver %s\n" 9.9.9.9 8.8.8.8 >/etc/resolv.conf
-cat /etc/resolv.conf
+echo "nameserver 192.168.121.1" >/etc/resolv.conf
